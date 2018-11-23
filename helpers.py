43e94872b09d99d0a09ca14f2b3b43f28c8a0243
zach-fried <zach.friedenreich@gmail.com>
2018-11-22 23:25:41 -0500

    Refactor parsing and update process_emg.py to process all channels for a single trial.

---
+++ helpers.py
@@ -0,0 +1,72 @@
+import pandas as pd
+
+# Refactor to only have one import_emg function
+def import_MVC(file):
+    if 'MVC' in file:
+        emg_MVC = pd.read_excel(file)
+        print('SUCCESSFULLY IMPORTED MVC FILE.')
+        return emg_MVC
+    else:
+        print('ERROR: This file does not contain the subject\'s MVC.')
+
+def import_dynamic(file):
+    if 'Trimmed' in file:
+        emg_dynamic = pd.read_excel(file)
+        print('SUCCESSFULLY IMPORTED DYNAMIC FILE.')
+        return emg_dynamic
+    else:
+        print('ERROR: This file is not a dynamic trial.')
+
+# TO DO: Think of algorithm for processing. Do you want to
+# take in all right side with one function and all left side
+# with a separate function? Need to associate proper MVC file
+# with each muscle group.
+
+def parse_emg(emg_MVC_RQuad, emg_MVC_RHam, emg_MVC_LQuad, 
+emg_MVC_LHam, emg_dynamic):
+
+    muscles_key = {'RVM': 19, 'RRec': 20, 'RVL': 21, 'RSemi': 22, 
+    'RBic': 23, 'LVM': 24, 'LRec': 25, 'LVL': 26, 'LSemi': 27, 
+    'LBic': 28}
+
+    emg_MVC_dict = dict()
+    emg_dynamic_dict = dict()
+
+    time_dynamic = emg_dynamic['File_Type:'][10:]
+    time = len(time_dynamic)
+
+    for k, v in muscles_key.items():
+        #print(k)
+        if any(['RVM' in k, 'RRec' in k, 'RVL' in k]):
+            #print('R QUAD MVC FILE')
+            emg_MVC_dict[k] = emg_MVC_RQuad[f'Unnamed: {v}'][10:(time + 10)]
+        elif any(['RSemi' in k, 'RBic' in k]):
+            #print('R HAM MVC FILE')
+            emg_MVC_dict[k] = emg_MVC_RHam[f'Unnamed: {v}'][10:(time + 10)]
+        elif any(['LVM' in k, 'LRec' in k, 'LVL' in k]):
+            #print('L QUAD MVC FILE')
+            emg_MVC_dict[k] = emg_MVC_LQuad[f'Unnamed: {v}'][10:(time + 10)]
+        else:
+            #print('L HAM MVC FILE')
+            emg_MVC_dict[k] = emg_MVC_LHam[f'Unnamed: {v}'][10:(time + 10)]
+
+        emg_dynamic_dict[k] = emg_dynamic[f'Unnamed: {v}'][10:]
+    
+    return emg_MVC_dict, emg_dynamic_dict, time_dynamic
+
+"""
+TESTING
+
+emg_MVC_RQuad = import_MVC('LX1511MVCRQuad01.xlsx')
+emg_MVC_RHam = import_MVC('LX1511MVCRHam01.xlsx')
+emg_MVC_LQuad = import_MVC('LX1511MVCLQuad01.xlsx')
+emg_MVC_LHam = import_MVC('LX1511MVCLHam01.xlsx')
+emg_dynamic = import_dynamic('Trimmed_LX1511eT08R1.xlsx')
+
+emg_MVC_dict, emg_dynamic_dict, time = parse_emg(emg_MVC_RQuad, emg_MVC_RHam, emg_MVC_LQuad, emg_MVC_LHam, emg_dynamic)
+"""
+
+    
+    
+
+    
--- process_emg.py
+++ process_emg.py
@@ -2,18 +2,19 @@ import math
 import pandas as pd
 from scipy import signal
 import matplotlib.pyplot as plt
-from import_emg import import_MVC, import_dynamic
+from helpers import import_MVC, import_dynamic, parse_emg
 
-# import MVC file
-emg_MVC = import_MVC('LX1511MVCRQuad01.xlsx')
+# import MVC files
+emg_MVC_RQuad = import_MVC('LX1511MVCRQuad01.xlsx')
+emg_MVC_RHam = import_MVC('LX1511MVCRHam01.xlsx')
+emg_MVC_LQuad = import_MVC('LX1511MVCLQuad01.xlsx')
+emg_MVC_LHam = import_MVC('LX1511MVCLHam01.xlsx')
 
 # import dynamic trial
 emg_dynamic = import_dynamic('Trimmed_LX1511eT08R1.xlsx')
 
 # parse R Rectus data and time vector from trial for testing
-time_dynamic = emg_dynamic['File_Type:'][10:]
-emg_RRec_MVC = emg_MVC['Unnamed: 20'][10:(len(time_dynamic)+10)]
-emg_RRec_dynamic = emg_dynamic['Unnamed: 20'][10:]
+emg_MVC_dict, emg_dynamic_dict, time = parse_emg(emg_MVC_RQuad, emg_MVC_RHam, emg_MVC_LQuad, emg_MVC_LHam, emg_dynamic)
 
 """
 # plot raw data
@@ -32,11 +33,15 @@ cutoff_high = 30
 cutoff_low = 4
 
 # high pass filter data
-b_MVC, a_MVC = signal.butter(4, cutoff_high/nyq, btype='highpass')
-emg_RRec_MVC_high = signal.filtfilt(b_MVC, a_MVC, emg_RRec_MVC)
+b, a = signal.butter(4, cutoff_high/nyq, btype='highpass')
 
-b_dyn, a_dyn = signal.butter(4, cutoff_high/nyq, btype='highpass')
-emg_RRec_dyn_high = signal.filtfilt(b_dyn, a_dyn, emg_RRec_dynamic)
+emg_MVC_high = dict()
+emg_dyn_high = dict()
+
+for k in emg_MVC_dict:
+
+    emg_MVC_high[k] = signal.filtfilt(b, a, emg_MVC_dict[k])
+    emg_dyn_high[k] = signal.filtfilt(b, a, emg_dynamic_dict[k])
 
 """
 # plot high pass filtered data
@@ -49,32 +54,43 @@ plt.show()
 """
 
 # full wave rectify data
-emg_RRec_MVC_rect = abs(emg_RRec_MVC_high)
-emg_RRec_dyn_rect = abs(emg_RRec_dyn_high)
+emg_MVC_rect = dict()
+emg_dyn_rect = dict()
+
+for k in emg_MVC_dict:
+    emg_MVC_rect[k] = abs(emg_MVC_high[k])
+    emg_dyn_rect[k] = abs(emg_dyn_high[k])
 
 # low pass filter data to calculate linear envelope
-d_MVC, c_MVC = signal.butter(4, cutoff_low/nyq, btype='lowpass')
-emg_RRec_MVC_env = signal.filtfilt(d_MVC, c_MVC, emg_RRec_MVC_rect)
+d, c = signal.butter(4, cutoff_low/nyq, btype='lowpass')
 
-d_dyn, c_dyn = signal.butter(4, cutoff_low/nyq, btype='lowpass')
-emg_RRec_dyn_env = signal.filtfilt(d_dyn, c_dyn, emg_RRec_dyn_rect)
+emg_MVC_env = dict()
+emg_dyn_env = dict()
 
+for k in emg_MVC_dict:
+    emg_MVC_env[k] = signal.filtfilt(d, c, emg_MVC_rect[k])
+    emg_dyn_env[k] = signal.filtfilt(d, c, emg_dyn_rect[k])
+"""
 # plot linear envelopes
 fig_env = plt.figure(dpi=200)
 axes_env = fig_env.add_axes([0.1,0.1,0.8,0.8])
-axes_env.plot(time_dynamic, emg_RRec_MVC_env, 'b')
-axes_env.plot(time_dynamic, emg_RRec_dyn_env, 'r')
+axes_env.plot(time, emg_MVC_env['RRec'], 'b')
+axes_env.plot(time, emg_dyn_env['RRec'], 'r')
 axes_env.set_title('Linear Envelope')
 plt.show()
-
+"""
 # normalize dynamic trial to MVC
-emg_RRec_dyn_norm = (emg_RRec_dyn_env / emg_RRec_MVC_env) * 100
+emg_dyn_norm = dict()
 
+for k in emg_MVC_dict:
+    emg_dyn_norm[k] = (emg_dyn_env[k] / emg_MVC_env[k]) * 100
+"""
 # plot normalized data as percentage of MVC
 fig_norm = plt.figure(dpi=200)
 axes_norm = fig_norm.add_axes([0.1,0.1,0.8,0.8])
-axes_norm.plot(time_dynamic, emg_RRec_dyn_norm, 'c')
+axes_norm.plot(time, emg_dyn_norm['RRec'], 'c')
 axes_norm.set_title('Normalized Data?')
 plt.show()
+"""
 
-print(emg_RRec_dyn_norm.max())
+pd.DataFrame(emg_dyn_norm).to_excel('test.xlsx')
